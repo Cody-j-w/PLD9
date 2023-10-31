9be1a10f5d345eaaf0270dad60c8471465439a10
Your Name <you@example.com>
2023-10-31 11:10:32 -0700

    Adding my working C file.

---
+++ addstuff.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+ * addstuff - add a string and a number together
+ * @string: the string to be printed 
+ * @number: the number to be printed 
+ */
+void addstuff(const char* string, int number)
+{
+	printf("%s %d\n", string, number);
+}
+
